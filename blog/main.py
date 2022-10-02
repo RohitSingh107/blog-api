99a07bb3a01c64d360a9d250f31f79f9d71ed6e9
Rohit <RohitSinghEmail@protonmail.com>
2022-10-02 22:57:48 +0530

    db setuped

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,44 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from pydantic import BaseModel
-from . import schemas
+from . import schemas, models
+from .database import SessionLocal, engine
+from sqlalchemy.orm import Session
+
+
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
 
-@app.post('/blog')
-def create(request: schemas.Blog):
-    return request
-    return {'title': request.title, 'body': request.body}
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+
+
+@app.get('/blog/{id}', status_code=200)
+def all(id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} is not available")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f"Blog with id {id} is not available"}
+    return blog
