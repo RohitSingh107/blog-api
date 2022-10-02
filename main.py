a5c1496a818221cfa951407e76b5da4208944e2f
Rohit <RohitSinghEmail@protonmail.com>
2022-10-02 19:31:23 +0530

    blog

---
+++ main.py
@@ -0,0 +1,50 @@
+
+from typing import Optional
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return {'data': 'All blogs'}
+
+
+@app.get('/blog')
+def blog(limit: int = 10, published: bool = False, sort: Optional[str] = None):
+    if published:
+        return {"data": f"{limit} published blogs from db list"}
+    else:
+        return {"data": f"{limit} blogs from db list"}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {"data": id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int, limit=10):
+    return {'data': {'1', '2', '3'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f"Blog is created with title as {request.title}"}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, h
