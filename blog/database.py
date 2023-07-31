b84ac5faf3f0a2d28a84af1fcc25681f5eb5608b
Rohit <RohitSinghEmail@protonmail.com>
2023-07-31 23:11:55 +0530

    postgresql integrated

--- blog/database.py
+++ blog/database.py
@@ -1,15 +1,13 @@
-from sqlite3 import connect
 from sqlalchemy import create_engine
 
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/blog_psql.db"
+# SQLALCHEMY_DATABASE_URL = "/run/postgresql/.s.PGSQL.5432/python_db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
-    "check_same_thread": False
-})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
